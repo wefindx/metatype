c41009d0bada2fadd937f3505fc98b4647082913
Mindey <mindey@qq.com>
2019-02-21 04:55:02 +0000

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='metatype',
-    version='0.0.1',
+    version='0.0.2',
     description='Implementation of base dict types for metaformat versions.',
     long_description=long_description,
     long_description_content_type='text/markdown',
