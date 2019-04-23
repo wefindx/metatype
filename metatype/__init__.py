f6ffd0e98dbc4bf2ff205b33fe51944e336cdb69
Mindey <mindey@qq.com>
2019-04-23 07:27:56 +0000

    use underscores to avoid property collision

--- metatype/__init__.py
+++ metatype/__init__.py
@@ -10,6 +10,11 @@ from metatype import config
 from typology.utils import slug
 from metawiki import url2ext
 
+PRIORITY_SEQUENCE = [
+    MFT1_KEYMAP,
+    JSONLD_KEYMAP,
+    DEFACTO_KEYMAP
+]
 
 class Dict(dict, metaclass=PropertyMeta):
     '''
@@ -33,88 +38,98 @@ class Dict(dict, metaclass=PropertyMeta):
     def drives(cls):
         return cls._DRIVES
 
+
+    def set_id(self):
+        self._id = None
+        for keymap in PRIORITY_SEQUENCE:
+            if keymap.get('_id') in self.keys():
+                self.id = self.get(keymap.get('_id'))
+                break
+
+    def set_type(self):
+        self._type = None
+        for keymap in PRIORITY_SEQUENCE:
+            if keymap.get('_type') in self.keys():
+                self._type = self.get(keymap.get('_type'))
+                break
+
+    def set_auth(self):
+        self._auth = None
+        for keymap in PRIORITY_SEQUENCE:
+            if keymap.get('_auth') in self.keys():
+                self._auth = self.get(keymap.get('_auth'))
+                break
+
+    def set_intent(self):
+        self._intent = None
+        for keymap in PRIORITY_SEQUENCE:
+            if keymap.get('_intent') in self.keys():
+                self._intent = self.get(keymap.get('_intent'))
+                break
+
+    def set_drive(self):
+        self._drive = None
+        for keymap in PRIORITY_SEQUENCE:
+            if keymap.get('_drive') in self.keys():
+                self._drive = self.get(keymap.get('_drive'))
+                break
+
     def initialize(self):
-        PRIORITY_SEQUENCE = [
-            MFT1_KEYMAP,
-            JSONLD_KEYMAP,
-            DEFACTO_KEYMAP
-        ]
-
-        self.id = None
-        for keymap in PRIORITY_SEQUENCE:
-            if keymap.get('id') in self.keys():
-                self.id = self.get(keymap.get('id'))
-                break
-
-        self.type = None
-        for keymap in PRIORITY_SEQUENCE:
-            if keymap.get('type') in self.keys():
-                self.type = self.get(keymap.get('type'))
-                break
-
-        self.auth = None
-        for keymap in PRIORITY_SEQUENCE:
-            if keymap.get('auth') in self.keys():
-                self.auth = self.get(keymap.get('auth'))
-                break
-
-        self.intent = None
-        for keymap in PRIORITY_SEQUENCE:
-            if keymap.get('intent') in self.keys():
-                self.intent = self.get(keymap.get('intent'))
-                break
-
-        # if not hasattr(self, 'drive'):
-        #     self.drive = None
-        #     for keymap in PRIORITY_SEQUENCE:
-        #         if keymap.get('drive') in self.keys():
-        #             self.drive = self.get(keymap.get('drive'))
-        #             break
+        self.set_id()
+        self.set_type()
+        self.set_auth()
+        self.set_intent()
+        self.set_drive()
 
     def __init__(self, *args, **kwargs):
         self.update(*args, **kwargs)
         self.initialize()
 
-    def get_filename(self):
-        if hasattr(self, 'id'):
-            if self.id is not None:
-                s = slug(self.id)
-                # POSSIBILITY: to use file extensions to denote short form of type, e.g.:
-                #       https-hello-world._Item
-                #       https-hello-world.xlsx._Table
-                #       https-hello-world.::._Tree
-
-                extension = '.yaml'
-
-                sub_extension = ''
-
-                if hasattr(self, 'type'):
-                    if isinstance(self.type, str):
-                        if self.type.startswith('http'):
-                            sub_extension = '.' + url2ext(self.type)
-
-                if sub_extension:
-                    extension = sub_extension + extension
-
-                fname = s[:config.FILENAME_LENGTH_LIMIT-len(extension)]+extension
-                return fname
-
     def get_filedir(self):
-        if hasattr(self, 'profile'):
-            if self.profile is not None:
-                DATA_PATH = os.path.join(config.DATA_DIR, self.profile, type(self).__name__)
-            else:
-                DATA_PATH = os.path.join(config.DATA_DIR, 'default', type(self).__name__)
+        if not hasattr(self, '_drive'):
+            self.set_drive()
 
-            if not os.path.exists(DATA_PATH):
-                os.makedirs(DATA_PATH)
+        if self._drive is not None:
+            DATA_PATH = os.path.join(config.DATA_DIR, self._drive, type(self).__name__)
+        else:
+            DATA_PATH = os.path.join(config.DATA_DIR, 'default', type(self).__name__)
+            print("Since the _drive or profile is not provided, using default directory {}. The object will be non-resumable.".format(DATA_PATH))
 
-            return DATA_PATH
+        if not os.path.exists(DATA_PATH):
+            os.makedirs(DATA_PATH)
+
+        return DATA_PATH
+
+    def get_extension(self):
+        extension = '.yaml'
+        sub_extension = '.'
+
+        if hasattr(self, '_type'):
+            if isinstance(self._type, str):
+                if self._type.startswith('http'):
+                    sub_extension += url2ext(self._type)
+
+        extension = sub_extension + extension
+
+    def get_filename(self):
+        if not hasattr(self, '_id'):
+            self.set_id()
+
+        if self._id is not None:
+            slg = slug(self._id)
+            ext = self.get_extension()
+            fname = slg[config.FILENAME_LENGTH_LIMIT-len(ext):]+ext
+
+        else:
+            raise Exception("Can't determine filename without _id. Assign '-' or 'url' key to data, and try again.")
+
+        return fname
+
+    def get_savepath(self):
+        dn = self.get_filedir()
+        fn = self.get_filename()
+        return os.path.join(dn, fn)
 
     def save(self):
-        path = os.path.join(
-            self.get_filedir(),
-            self.get_filename()
-        )
-        with open(path, 'w') as f:
+        with open(get_savepath(), 'w') as f:
             f.write(yaml.dump(self))
