015490deaad10ab9c3163d0c8e72db683530468a
Mindey <mindey@qq.com>
2019-04-23 09:17:35 +0000

    fix

--- metatype/__init__.py
+++ metatype/__init__.py
@@ -16,6 +16,7 @@ PRIORITY_SEQUENCE = [
     DEFACTO_KEYMAP
 ]
 
+
 class Dict(dict, metaclass=PropertyMeta):
     '''
     Accepts: any dictionary, that optionally has keys specifying:
@@ -33,6 +34,21 @@ class Dict(dict, metaclass=PropertyMeta):
 
     '''
     _DRIVES = []
+
+    @classmethod
+    def get_drive(cls, drive, profile):
+        '''
+        A helper function to determine the drive name from:
+
+        drive: drive instance,
+        profile: profile name.
+
+        Intended to be called in drivers, while drive instance is available, to set '_drive' name attribute.
+        '''
+        if drive.caller_module.__name__+':' not in drive.profile:
+            return '{}:{}'.format(drive.caller_module.__name__, drive.profile)
+        else:
+            return drive.profile
 
     @classproperty
     def drives(cls):
